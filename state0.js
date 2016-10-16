14352a915c6b0ecdf743b855f57047e2c7cf8fbd
Dan <you@example.com>
2016-10-16 20:42:41 +0100

    finish state 0

--- state0.js
+++ state0.js
@@ -1,30 +1,50 @@
-var demo = {}, centerX = 1500 / 2, centerY = 1000 / 2, adam, speed=4;
+var demo = {}, centerX = 1500 / 2, centerY = 1000 / 2, adam, speed=6;
 demo.state0 = function() {};
 demo.state0.prototype = {
     preload: function() {
-		game.load.image('adam','assets/sprites/adam.png');
+		game.load.spritesheet('adam','assets/spritesheets/adamSheet.png', 260, 500);
+        game.load.image('tree','assets/backgrounds/treeBG.png');
 	},
 	create: function() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
 		game.stage.backgroundColor = '#ff5500';
 		console.log('state 0');
 
 		addChangeStateEventListeners();
+        game.world.setBounds(0, 0, 2013, 1000);
 		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		
+        var treeBG = game.add.sprite(0, 0, 'tree');
+        
 		adam = game.add.sprite(centerX, centerY, 'adam');
 		adam.anchor.setTo(0.5, 0.5);
-		
+        adam.scale.setTo(0.5, 0.5);
+        game.physics.enable(adam);
+        adam.body.collideWorldBounds = true;
+        adam.animations.add('walk', [0,1,2,3,4,5]);
+		game.camera.follow(adam);
+        game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0, 600, 1000);
 	 },
 	update: function() {
 		if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-			adam.x += speed;
+			adam.scale.setTo(0.5, 0.5);
+            adam.x += speed;
+            adam.animations.play('walk', 14, true);
 			
 		}else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-			adam.x -= speed;
-			
+			adam.scale.setTo(-0.5, 0.5);
+            adam.x -= speed;
+			adam.animations.play('walk', 14, true);
 		}
+        else{
+            adam.animations.stop('walk');
+            adam.frame = 0;
+        }
         if(game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
 			adam.y -= speed;
+            if(adam.y < 487){
+                adam.y = 487;
+            }
 			
 		}else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
 			adam.y += speed;
